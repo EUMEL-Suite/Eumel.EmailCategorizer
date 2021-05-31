b957c5a3c9c857b4f031f82cdc934e16baeaa9f4
Thomas Ley <thomas-ley@outlook.de>
2021-05-31 18:47:43 +0200

    screenshot for documentation added and automatically created. Test project to show UI added.

---
+++ Eumel.EmailCategorizer/Eumel.EmailCategorizer.WpfUI.Tests/EmailSubjectWindow_Should.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using NUnit.Framework;
+
+namespace Eumel.EmailCategorizer.WpfUI.Tests
+{
+    [TestFixture]
+    // ReSharper disable once InconsistentNaming
+    public class EmailSubjectWindow_Should
+    {
+        private const string Assets = "C:\\Workspaces\\Eumel.EmailCategorizer\\Assets\\";
+
+        public static void CreateBitmapFromVisual(Visual target, string fileName)
+        {
+            if (target == null || string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(target);
+
+            RenderTargetBitmap renderTarget = new RenderTargetBitmap((Int32)bounds.Width, (Int32)bounds.Height, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual visual = new DrawingVisual();
+
+            using (DrawingContext context = visual.RenderOpen())
+            {
+                VisualBrush visualBrush = new VisualBrush(target);
+                context.DrawRectangle(visualBrush, null, new Rect(new Point(), bounds.Size));
+            }
+
+            renderTarget.Render(visual);
+            PngBitmapEncoder bitmapEncoder = new PngBitmapEncoder();
+            bitmapEncoder.Frames.Add(BitmapFrame.Create(renderTarget));
+            using (Stream stm = File.Create(fileName))
+            {
+                bitmapEncoder.Save(stm);
+            }
+        }
+
+        [Test]
+        [Explicit("this opens a UI")]
+        [Apartment(ApartmentState.STA)]
+        public void Open_Window()
+        {
+            var window = new EmailSubjectWindow()
+            {
+                Subject = new EnhancedSubject("[Eumel] Regression Test Pattern")
+            };
+            window.Show();
+            CreateBitmapFromVisual(window, Assets + "eumel_subjecteditor.png");
+            window.Hide();
+            
+            window.ShowDialog();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 The EUMEL email categorizer is an outlook addin to tag or categorize email in its subject. The goal is sending email with an prefixed subject to
 categorize emails before it is sent.
 
+![Subject Editor](./Assets/eumel_subjecteditor.png)
+
 
